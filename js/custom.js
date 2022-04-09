3681e9f2554069a242039ab261a04fa0447d3977
samudio12 <45163011+samudio12@users.noreply.github.com>
2022-04-09 20:12:01 +0200

    first batch fixes

--- js/custom.js
+++ js/custom.js
@@ -1,46 +1,43 @@
-
- /* jQuery Pre loader
+/* jQuery Pre loader
   -----------------------------------------------*/
-$(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets    
+$(window).load(function () {
+    $('.preloader').fadeOut(1000); // set duration in brackets
 });
 
-
-$(document).ready(function() {
-
-  /* Home Slideshow Vegas
+$(document).ready(function () {
+    /* Back top
   -----------------------------------------------*/
-  /*$(function() {
-    $('body').vegas({
-        slides: [
-            { src: 'images/slide-1.jpg' },
-            { src: 'images/slide-2.jpg' }
-        ],
-        timer: false,
-        transition: [ 'zoomOut', ]
-    });
-  });*/
-
-
-   /* Back top
-  -----------------------------------------------*/
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(this).scrollTop() > 200) {
-        $('.go-top').fadeIn(200);
+            $('.go-top').fadeIn(200);
         } else {
-          $('.go-top').fadeOut(200);
+            $('.go-top').fadeOut(200);
         }
-        });   
-        // Animate the scroll to top
-      $('.go-top').click(function(event) {
+    });
+    // Animate the scroll to top
+    $('.go-top').click(function (event) {
         event.preventDefault();
-      $('html, body').animate({scrollTop: 0}, 300);
-      })
-      
+        $('html, body').animate({ scrollTop: 0 }, 300);
+    });
 
-  /* wow
+    /* wow
   -------------------------------*/
-  new WOW({ mobile: false }).init();
+    new WOW({ mobile: false }).init();
 
-  });
+    // isotop sortable grid
 
+    const $grid = $('.sortable-grid');
+    $grid.isotope({
+        itemSelector: '.grid-item',
+        //  layoutMode: 'fitRows',
+        percentPosition: true
+    });
+
+    setTimeout(() => $grid.isotope('layout'), 1500);
+
+    // filter items on button click
+    $('.filter-button-group').on('click', 'button', function () {
+        var filterValue = $(this).attr('data-filter');
+        $grid.isotope({ filter: filterValue });
+    });
+});
