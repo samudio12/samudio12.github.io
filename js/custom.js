2252f00279cb0e39e04d5cb9be0a3677e062abff
samudio12 <45163011+samudio12@users.noreply.github.com>
2022-04-09 20:12:02 +0200

    last gh

--- js/custom.js
+++ js/custom.js
@@ -40,4 +40,42 @@ $(document).ready(function () {
         var filterValue = $(this).attr('data-filter');
         $grid.isotope({ filter: filterValue });
     });
+
+    $('[data-fancybox]').fancybox({
+        caption: function (instance, item) {
+            console.log($(this).find('.project-caption'));
+            return $(this).find('.project-caption').html();
+        }
+    });
+
+    $('#submit').click(function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const from_name = $('#from_name').val();
+        const reply_to = $('#reply_to').val();
+        const message = $('#message').val();
+        const data = {
+            service_id: '-',
+            template_id: '-',
+            user_id: '-',
+            template_params: {
+                from_name,
+                reply_to,
+                message,
+            }
+        };
+
+
+        $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json'
+        }).done(function() {
+            alert('Your mail is sent!');
+        }).fail(function(error) {
+            alert('Oops... ' + JSON.stringify(error));
+        });
+
+    });
 });
